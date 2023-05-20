2d0fab93180fab3bad72a858ed42af4aa7495682
Christian Grossmüller <chgad.games@gmail.com>
2023-05-20 14:00:56 +0200

    Implement TaxonomyAndLivingSerializer.

--- planty_plant_content/serializers.py
+++ planty_plant_content/serializers.py
@@ -1,12 +1,28 @@
 
-from .models import Plant
+from .models import Plant, Taxonomy, Living
 from planty_content.serializers import DisplayNameModelSerializer
 from rest_framework.serializers import Serializer
 
 
+class LivingSerializer(DisplayNameModelSerializer):
+    class Meta:
+        model = Living
+        fields = "__all__"
+        depth = 1
+
+
+class TaxonomyAndLivingSerializer(DisplayNameModelSerializer):
+    living = LivingSerializer()
+
+    class Meta:
+        model = Taxonomy
+        exclude = ["plant"]
+        depth = 1
+
+
 class PlantSerializer(DisplayNameModelSerializer):
 
-    living = Serializer()
+    living = TaxonomyAndLivingSerializer()
     ecology_andd_natlocation = Serializer()
     appearance = Serializer()
     application = Serializer()
