5ab1fdbf04819c4082e4c551d2699f0325688289
Christian Grossmüller <chgad.games@gmail.com>
2023-05-20 14:15:15 +0200

    Add EcologyAndNatlocationSerializer with all deeper Serializers.

--- planty_plant_content/serializers.py
+++ planty_plant_content/serializers.py
@@ -1,5 +1,5 @@
 
-from .models import Plant, Taxonomy, Living
+from .models import Plant, Taxonomy, Living, EcologyAndNatLocation, NatOccurence, ZeigerValues, NatBehavior
 from planty_content.serializers import DisplayNameModelSerializer
 from rest_framework.serializers import Serializer
 
@@ -20,10 +20,45 @@ class TaxonomyAndLivingSerializer(DisplayNameModelSerializer):
         depth = 1
 
 
+class NatOccurenceSerializer(DisplayNameModelSerializer):
+    
+    class Meta:
+        model = NatOccurence
+        fields = "__all__"
+        depth = 1
+
+
+class ZeigerValuesSerializer(DisplayNameModelSerializer):
+    
+    class Meta:
+        model = ZeigerValues
+        fields = "__all__"
+        depth = 1
+
+
+class NatBehaviorSerializer(DisplayNameModelSerializer):
+    zeiger_value = ZeigerValuesSerializer()
+
+    class Meta:
+        model = NatBehavior
+        fields = "__all__"
+        depth = 1
+
+
+class EcologyAndNatLocationSerializer(DisplayNameModelSerializer):
+    nat_occ = NatOccurenceSerializer()
+    nat_behavior = NatBehaviorSerializer()
+
+    class Meta:
+        model = EcologyAndNatLocation
+        exclude = ["plant"]
+        depth = 1
+
+
 class PlantSerializer(DisplayNameModelSerializer):
 
     living = TaxonomyAndLivingSerializer()
-    ecology_and_natlocation = Serializer()
+    ecology_and_natlocation = EcologyAndNatLocationSerializer()
     appearance = Serializer()
     application = Serializer()
     plantation_and_creation = Serializer()
