454f0b32977300d49968580a08fa39faca09711f
Christian Grossmüller <chgad.games@gmail.com>
2023-05-20 13:42:21 +0200

    List first level of Serializers.

--- planty_plant_content/serializers.py
+++ planty_plant_content/serializers.py
@@ -1,9 +1,20 @@
 
 from .models import Plant
 from planty_content.serializers import DisplayNameModelSerializer
+from rest_framework.serializers import Serializer
 
 
 class PlantSerializer(DisplayNameModelSerializer):
+
+    living = Serializer()
+    ecology_andd_natlocation = Serializer()
+    appearance = Serializer()
+    application = Serializer()
+    plantation_and_creation = Serializer()
+    reproduction_and_production = Serializer()
+    usability = Serializer()
+    disease = Serializer()
+    general_information = Serializer()
 
     class Meta:
         model = Plant
