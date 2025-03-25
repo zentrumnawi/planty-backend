b3704dc87c24aa58911d1062a0dd9306478dd71f
Lisakarolina <w.hello@web.de>
2025-03-25 21:06:21 +0100

    explicitly set default_auto_field

--- planty_plant_content/apps.py
+++ planty_plant_content/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PlantyPlantContentConfig(AppConfig):
     name = "planty_plant_content"
     verbose_name = "Kulturpflanzen"
+    default_auto_field = "django.db.models.AutoField"
