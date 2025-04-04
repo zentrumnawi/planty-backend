c09b4bd40a696302a9f34f91c60a69696cb33bb9
Lisakarolina <w.hello@web.de>
2025-04-04 11:27:42 +0200

    Fix formatting of config

--- config/settings/test.py
+++ config/settings/test.py
@@ -87,11 +87,7 @@ EMAIL_BACKEND = env(
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-    }
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql_psycopg2",}}
 
 # CACHING
 CACHES = {
